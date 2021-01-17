9ee78db2c7afb39732919e07a58de4e3f82bfe80
ayiamco <josephachonu@gmail.com>
2021-01-17 10:02:02 +0100

    bug fix: fixed register component validation errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {HashRouter as Router,Route,Switch} from "react-router-dom"
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom"
 import './App.css';
 import Login from './components/login/login'
 import Home from './components/home/home'
@@ -9,9 +9,12 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Login}></Route>
+        <Route exact path="/" component={Register}></Route>
+        <Route exact path="/login" component={Login}></Route>
         <Route exact path="/home" component={Home}></Route>
         <Route exact path="/register" component={Register}></Route>
+        
+        
         <Route path='/*' >
           <h1>Error</h1>
         </Route>
--- src/components/register/registerform.js
+++ src/components/register/registerform.js
@@ -11,13 +11,30 @@ function RegisterForm(){
         phoneNumber:"",
         address:""
     })
-    const [isbuttonDisabled,setIsButtonDisabled]=useState(true);
-    const [showMessage,setShowMessage]=useState(false);
+
+    const [boolenStates,setBoolenStates]=useState({
+        "isPasswordMatch":true,
+        "isValidEmail":true,
+        "shouldButtonDisable":true,
+        "isRequestSent":false
+    });
+    
+    const [errorMessage,setErrorMessage]=useState("Error: Please check your network connection")
+    const [networkError,setnetworkError]=useState("none")
     const history=useHistory();
 
+    function validateEmail(email) {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
     const handleLoginForm =(e)=>{
-     e.preventDefault()
-     
+        e.preventDefault()
+
+        //prevent btn from being clicked while request is sent
+        setBoolenStates(prev=>{
+            return {...prev,"isRequestSent":true}
+        })
+        
         fetch("https://localhost:44322/api/laundry/register",{
             method:"POST",
             headers:{
@@ -53,12 +70,39 @@ function RegisterForm(){
             .then(res=>{
                 localStorage.setItem("token",res.token)
                 history.push("/home")
+
+                //request cycle has ended 
+                setBoolenStates(prev=>{
+                    return {...prev,"isRequestSent":false}
+                    });
                 })
+            }
+            else if(res.statusCode==="400"){
+                setErrorMessage(()=> {return res.message})
+                setnetworkError( "block")
+                setBoolenStates(prev=>{
+                    return {...prev,"shouldButtonDisable":false}
+                })
+                
+                console.log("got here",errorMessage)
+            }
+            else if(res.statusCode==="500"){
+                setErrorMessage(" Server Error: there seem to be an error with the server ")
+                setnetworkError("block")
+                setBoolenStates(prev=>{
+                    return {...prev,"shouldButtonDisable":false}
+                })
+                
             }
             
         })
         .catch((err)=>{
-            console.log(err)
+            setErrorMessage(" Network Error: please check your network ")
+            setnetworkError("block")
+            setBoolenStates(prev=>{
+                    return {...prev,"shouldButtonDisable":false}
+                })
+            
         })
 
     }
@@ -78,46 +122,90 @@ function RegisterForm(){
     }
 
     useEffect( () =>{
-        console.log(formData.password.length,formData.confirmPassword.length)
+        
         let isFormDataValid=true;
-       Object.keys(formData).forEach(key=>{
-           //validate all feilds are not empty 
+        Object.keys(formData).forEach(key=>{
+
+           //validate that all feilds are not empty 
             if(formData[key]===""){
                 isFormDataValid=false;
             }
+
             //validate that passwords matched
-            if(formData.password !== formData.confirmPassword && formData.confirmPassword !==""){
+            if(formData.password !== formData.confirmPassword && formData.confirmPassword !=="" && key==="confirmPassword"){
                 isFormDataValid=false;
-                setShowMessage(true);
+                setBoolenStates(prev=>{
+                    return {...prev,"isPasswordMatch":false}
+                })
+                
+                
             }
-            else{
-                 setShowMessage(false);
+            else if(key==="confirmPassword"){
+                 setBoolenStates(prev=>{
+                    return {...prev,"isPasswordMatch":true}
+                })
             }
+
+            //check email validity
+            if( validateEmail(formData.username)===false && formData.username!=="" && key==="username"){
+                setBoolenStates(prev=>{
+                    return {...prev,"isValidEmail":false}
+                })
+                isFormDataValid=false;
+            }
+            else if(validateEmail(formData.username)===true && formData.username!=="" && key==="username"){
+                setBoolenStates(prev=>{
+                    return {...prev,"isValidEmail":true}
+                })
+            }
+            else if(formData.username===""){
+                setBoolenStates(prev=>{
+                    return {...prev,"isValidEmail":true}
+                })
+                isFormDataValid=false;
+            }
+            
        })
-       if(isFormDataValid){setIsButtonDisabled(false)}
-       else{setIsButtonDisabled(true)}
+
+       //allow request submission depending on validation state
+       if(isFormDataValid){
+           setBoolenStates(prev=>{
+                    return {...prev,"shouldButtonDisable":false}
+                })
+        }
+       else{
+           setBoolenStates(prev=>{
+                    return {...prev,"shouldButtonDisable":true}
+                })
+        }
+        console.log(boolenStates)
+
     },[formData])
 
 
     return (
         <div className="LF-con">
-            
+            <p style={{display:networkError,color:"red",fontSize:"0.8em",paddingLeft:"2em"}}>{errorMessage}</p>
             <form onSubmit={handleLoginForm} >
                 <div>
                     <input type="text" name="laundryName"placeholder="Laundry Name " onChange={handleInput} value={formData.laundryName}></input>
                     <p></p>
                 </div>
                 <div>
-                    <input type="email" name="username"placeholder="Email Address" onChange={handleInput} value={formData.username}></input>
-                    <p></p>
+                     <input type="email" name="username"placeholder="Email Address" onChange={handleInput} value={formData.username}></input>
+                    {boolenStates.isValidEmail ? <p></p>:<p style={{color:"red",fontSize:"0.7em", paddingLeft:"2em"}}> Enter a valid Email</p>}
+                
                 </div>
+               
+                   
+
                 <div>
                     <input placeholder="Password" name="password" onChange={handleInput} value={formData.password}></input>
                     <p></p>
                 </div>
                 <div>
                     <input placeholder="ConfirmPassword" name="confirmPassword" onChange={handleInput} value={formData.confirmPassword}></input>
-                    {showMessage ? <p style={{color:"red",fontSize:"0.7em", paddingLeft:"2em"}}> passwords do not match</p>:<p></p>}
+                    {boolenStates.isPasswordMatch ? <p></p>:<p style={{color:"red",fontSize:"0.7em", paddingLeft:"2em"}}> passwords do not match</p>}
                 </div>
                 <div>
                     <input placeholder="Phone Number" name="phoneNumber" onChange={handleInput} value={formData.phoneNumber}></input>
@@ -127,7 +215,7 @@ function RegisterForm(){
                     <input placeholder="Address" name="address" onChange={handleInput} value={formData.address}></input>
                     <p></p>
                 </div>
-                <button disabled={isbuttonDisabled} >Create Account</button>
+                <button id={boolenStates.isRequestSent?"btn-waiting":""} disabled={boolenStates.shouldButtonDisable} >Create Account</button>
             </form>
         </div>
     )
